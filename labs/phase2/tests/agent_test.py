d37e1cb3087d42558c19eb39db6ddf3653801165
Justin Randall <justinran@google.com>
2025-12-07 00:19:06 +0000

    test: Added full test for phase 2.

--- labs/phase2/tests/agent_test.py
+++ labs/phase2/tests/agent_test.py
@@ -12,25 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright 2025 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import pytest
 
 import asyncio
 import logging
+
+from typing import AsyncGenerator
+from google.adk.events.event import Event
+from google.adk.agents import parallel_agent
 
 from dotenv import load_dotenv
 import google.auth
@@ -38,6 +28,11 @@ from google import genai
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
 from google.genai.types import Content, Part
+
+from opentelemetry import trace
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.sdk.trace import export
+from opentelemetry.sdk.trace import TracerProvider
 
 from agents.commander.agent import root_agent
 
@@ -47,12 +42,67 @@ logging.getLogger("google_adk").setLevel(logging.DEBUG)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
+# --- BEGIN MONKEY PATCH ---
+async def _merge_agent_run_patched(
+    agent_runs: list[AsyncGenerator[Event, None]],
+) -> AsyncGenerator[Event, None]:
+    """Merges the agent run event generator.
+
+    This implementation guarantees for each agent, it won't move on until the
+    generated event is processed by upstream runner.
+
+    Args:
+        agent_runs: A list of async generators that yield events from each agent.
+
+    Yields:
+        Event: The next event from the merged generator.
+    """
+    sentinel = object()
+    queue = asyncio.Queue()
+
+    # Agents are processed in parallel.
+    # Events for each agent are put on queue sequentially.
+    async def process_an_agent(events_for_one_agent):
+        try:
+            async for event in events_for_one_agent:
+                resume_signal = asyncio.Event()
+                await queue.put((event, resume_signal))
+                # Wait for upstream to consume event before generating new events.
+                await resume_signal.wait()
+        finally:
+            # Mark agent as finished.
+            await queue.put((sentinel, None))
+
+    async with asyncio.TaskGroup() as tg:
+        for events_for_one_agent in agent_runs:
+            tg.create_task(process_an_agent(events_for_one_agent))
+
+        sentinel_count = 0
+        # Run until all agents finished processing.
+        try:
+            while sentinel_count < len(agent_runs):
+                event, resume_signal = await queue.get()
+                # Agent finished processing.
+                if event is sentinel:
+                    sentinel_count += 1
+                else:
+                    yield event
+                    # Signal to agent that it should generate next event.
+                    resume_signal.set()
+        except GeneratorExit:
+            pass # Gracefully exit when the generator is closed.
+
+
+parallel_agent._merge_agent_run = _merge_agent_run_patched
+# --- END MONKEY PATCH ---
+
+
 load_dotenv()
 
 
 parameterized_test_data = [
     (
-        "Can you please find me 100 H100 GPUs?"
+        "Start the investigation. Find me 500 H100s."
     ),
 ]
 
@@ -65,6 +115,17 @@ os.environ["GOOGLE_GENAI_USE_VERTEXAI"] = "1"
 os.environ["GOOGLE_CLOUD_PROJECT"] = PROJECT_ID
 os.environ["GOOGLE_CLOUD_LOCATION"] = LOCATION
 
+os.environ["OTEL_SERVICE_NAME"] = "labs-phase1"
+os.environ["OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED"] = "true"
+os.environ["OTEL_INSTRUMENTATION_GENAI_CAPTURE_MESSAGE_CONTENT"] = "true"
+#os.environ["ADK_CAPTURE_MESSAGE_CONTENT_IN_SPANS"] = "false"
+
+provider = TracerProvider()
+processor = export.BatchSpanProcessor(
+    CloudTraceSpanExporter(project_id=PROJECT_ID)
+)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
 
 async def call_agent_async(prompt: str|Content):
     # Initialize the ADK runtime components
@@ -135,3 +196,4 @@ def test_run(prompt, run_number):
     )
 
     asyncio.run(call_agent_async(prompt))
+    
