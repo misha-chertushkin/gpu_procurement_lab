0f73d22b4e349d5967339dadbd665480e3786d1f
Justin Randall <justinran@google.com>
2025-12-07 00:20:26 +0000

    fix: Set the model to gemini-3

--- labs/phase2/src/tools/contract_analyzer.py
+++ labs/phase2/src/tools/contract_analyzer.py
@@ -21,8 +21,7 @@ from utils.config import config
 class ContractAnalyzer:
     def __init__(self):
         vertexai.init(project=config.PROJECT_ID, location=config.REGION)
-        self.model = GenerativeModel("gemini-2.5-pro")
-        #self.model = GenerativeModel("gemini-3-pro-preview")
+        self.model = GenerativeModel("gemini-3-pro-preview")
         self.storage_client = storage.Client()
 
     def analyze_contract_clause(self, doc_name: str, clause_type: str) -> str:
